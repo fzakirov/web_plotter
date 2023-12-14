b380ea434b1b06fe46fb6e99bc73cfbd8094eb65
Felix Zakirov <f_zakirov@vk.com>
2023-12-14 14:11:16 -0500

    Added display feature

--- plot_app/plotter/urls.py
+++ plot_app/plotter/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('plot/', views.plot, name='plot'),
     path('plot_result/', views.plot_result, name='plot_result'),
-    # path('display_data/', views.display_data, name='display_data'),
+    path('display_data/', views.display_data, name='display_data'),
 ]
--- plot_app/plotter/views.py
+++ plot_app/plotter/views.py
@@ -27,13 +27,6 @@ def plot_result(request):
     file_path = request.POST['file']
     df = pd.read_csv(file_path)
     df_selected = df[selected_columns]
-
-
-def plot_result(request):
-    selected_columns = request.POST.getlist('columns')
-    file_path = request.POST['file']
-    df = pd.read_csv(file_path)
-    df_selected = df[selected_columns]
     fig = px.box(df_selected,
              title = str(selected_columns),
              height = 800,
@@ -47,13 +40,9 @@ def plot_result(request):
     graph = fig.to_html(full_html=False)
     return render(request, 'plot_result.html', {'graph': graph})
 
-
-# def display_data(request):
-#     if request.method == 'POST' and request.FILES['file']:
-#         selected_columns = request.POST.getlist('columns')
-#         uploaded_file = request.FILES['file']
-#         df = pd.read_csv(uploaded_file)
-#         df_selected = df[selected_columns]
-#         return render(request, 'display_data.html', {'data': df_selected})
-#     else:
-#         return render(request, 'index.html', {'error': 'No file uploaded'})
+def display_data(request):
+    selected_columns = request.POST.getlist('columns')
+    file_path = request.POST['file']
+    df = pd.read_csv(file_path)
+    df_selected = df[selected_columns]
+    return render(request, 'display_data.html', {'data': df_selected})
