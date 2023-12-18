7134bcf0031d6e6469b59bb9ec74b33021b8bc1f
Felix Zakirov <f_zakirov@vk.com>
2023-12-17 21:51:54 -0500

    Added subsetting

--- plot_app/plotter/views.py
+++ plot_app/plotter/views.py
@@ -1,21 +1,12 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
-# Create your views here.
 import pandas as pd
 import plotly.express as px
-
+import os
 
 def index(request):
     return render(request, 'index.html')
-
-from django.core.files.storage import FileSystemStorage
-import tempfile
-from django.core.files.uploadhandler import TemporaryFileUploadHandler
-
-import io
-import os
-
-from django.core.files.storage import FileSystemStorage
 
 def plot(request):
     if request.method == 'POST' and request.FILES['file']:
@@ -25,23 +16,31 @@ def plot(request):
         file_path = fs.path(filename)
         df = pd.read_csv(file_path)
         columns = list(df.columns)
-        return render(request, 'plot.html', {'columns': columns, 'file': file_path})
+        conditions = [df.nunique().index[i] for i in range(len(df.nunique())) if df.nunique()[i]<=3]
+        subsets = []
+        for cond in conditions:
+            subsets.extend([cond + ":" + i for i in list(df[cond].unique())])
+        response = render(request, 'plot.html', {'columns': columns, 'subsets': subsets, 'file': file_path})
     else:
-        return render(request, 'index.html', {'error': 'No file uploaded'})
-
+        response = render(request, 'index.html', {'error': 'No file uploaded'})
+    #os.remove(file_path)
+    return(response)
 
 def plot_result(request):
     if request.method == 'POST':
         selected_columns = request.POST.getlist('selected_columns')
+        selected_subsets = request.POST.getlist('selected_subsets')
         file_path = request.POST['file']
         df = pd.read_csv(file_path)
+        for pair in selected_subsets:
+            df = df[df[pair.split(":")[0]] == pair.split(":")[1]]
         df_selected = df[selected_columns]
         fig = px.box(df_selected,
-                 title = str(selected_columns),
+                 title = "Columns: {}<br>Condition: {} N = {}".format(selected_columns, selected_subsets, len(df_selected)),
                  height = 800,
                  width = 1000,)
-
         fig.update_layout(
+        # title='<b></b>',
             font=dict(
                 size=20,  # Set the font size
             )
